c74a8fcf3359ff81d02cd23b0d64fa4d7224acad
tarellano <thomas.g.arellano@gmail.com>
2017-11-13 20:29:45 -0500

    Add prop typechecking and remove unused variables

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import promise from 'redux-promise-middleware';
 
 import reducer from './reducers/reducer';
