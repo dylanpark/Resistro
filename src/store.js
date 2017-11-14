7895acf07c7cd104e0bfde5f0588e6372b97b813
tarellano <thomas.g.arellano@gmail.com>
2017-11-13 20:41:02 -0500

    Remove outdated comment

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import promise from 'redux-promise-middleware';
 
 import reducer from './reducers/reducer';
 
-const middleware = applyMiddleware(promise(), thunk); //logger is just for dev purpose, it logs all the actions in the conosle when they are fired.
+const middleware = applyMiddleware(promise(), thunk);
 
 const store = createStore(reducer, middleware);
 export default store;
