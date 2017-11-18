870ddc8583f719c9a111158f629cdbd4d20eeec7
tarellano <thomas.g.arellano@gmail.com>
2017-11-18 15:18:25 -0500

    Add jest environment and fix place holder test

--- tests/page/components/color-input/color-input-test.js
+++ tests/page/components/color-input/color-input-test.js
@@ -1,7 +1,5 @@
 describe("Color Input", () => {
-    describe("component did mount", ()=> {
-        it("one should be one", ()=> {
-            expect(1).toBe(1);
-        });
+    it("one should be one", ()=> {
+        expect(1).toBe(1);
     });
 });
